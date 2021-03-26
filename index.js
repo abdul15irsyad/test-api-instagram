7751934a56641cee08a7f89446296f2131c676cb
abdul15irsyad <abdulirsyad15@gmail.com>
2021-03-26 23:47:04 +0700

    add cors

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
 require('./config/mongodb')
 
-const api = require('./api')
-const routers = require('./routers')
 const port = process.env.PORT || 3000
+const api = require('./api')
+const router = require('./router')
 
 app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.use('/', express.static('public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', api)
-app.use('/', routers)
+app.use('/', router)
 
 app.listen(port, () => console.log(`server running on port ${port}`))
