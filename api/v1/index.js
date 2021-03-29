59413446eaf79f1bcf856f0bddea8f72b28d1a48
abdul15irsyad <abdulirsyad15@gmail.com>
2021-03-29 17:47:01 +0700

    first commit

--- api/v1/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const user = require('./user')
-
-router.use('/user', user);
-
-module.exports = router;
--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-require('./config/mongodb')
 
 const port = process.env.PORT || 3000
-const api = require('./api')
 const router = require('./router')
 
 app.set('view engine', 'pug')
@@ -16,7 +14,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api', api)
 app.use('/', router)
 
 app.listen(port, () => console.log(`server running on port ${port}`))
